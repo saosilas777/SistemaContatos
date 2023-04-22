257cb19290706e5e81ffc579dbc32a3c83bb7850
Silas Dias <saosilas@hotmail.com>
2023-04-22 19:28:29 -0300

    ajustes pagina de login

---
+++ SistemaContatos/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaContatos.Models;
+
+namespace SistemaContatos.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View("Login", login);
+            }
+            catch (Exception)
+            {
+                TempData["ErrorMessage"] = $"Não foi possível efetuar seu login! erro:{e.Message}";
+                return RedirectToAction("Login", "Login");
+            }
+        }
+    }
+}
---
+++ SistemaContatos/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaContatos.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Digite um login")]
+        public string?  Login { get; set; }
+        [Required (ErrorMessage = "Digite uma senha")]
+        public string? Password { get; set; }
+    }
+}
